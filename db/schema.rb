b14c014a54a757ea4a60b071bce7b1eb72223c3a
Pieter Desender <Pieter_Desender@hotmail.com>
2014-11-24 17:59:09 +0100

    pre merge commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141118104528) do
+ActiveRecord::Schema.define(version: 20141119110022) do
+
+  create_table "presentations", force: true do |t|
+    t.string   "name"
+    t.string   "user"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",    null: false
