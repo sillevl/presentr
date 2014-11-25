0ddb12eae3efd7818b7ea18bb8853a929abd9eab
Stephen D'Hondt <stephendhondt@hotmail.be>
2014-11-25 10:28:29 +0100

    Add a defenition to enable slidenumbers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
 	def revealInitializeDefault
        javascript_include_tag "reveal_initialize_default.js" 
     end 
+
+    def enableSlideNumbers
+    	javascript_tag "Reveal.configure({ slideNumber: true })"
+    end
 end
