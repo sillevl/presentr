8f00ca922a928308da39ef017d849efe08222229
Stephen D'Hondt <stephendhondt@hotmail.be>
2014-12-03 15:48:37 +0100

    Added association

--- app/models/presentation.rb
+++ app/models/presentation.rb
@@ -1,5 +1,6 @@
 class Presentation < ActiveRecord::Base
 	belongs_to :user
+	belongs_to :course
 	has_many :slides
 	validates :name,  presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141126175859) do
+ActiveRecord::Schema.define(version: 20141203144244) do
+
+  create_table "courses", force: true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "presentations", force: true do |t|
     t.string   "name"
@@ -19,7 +26,10 @@ ActiveRecord::Schema.define(version: 20141126175859) do
     t.datetime "updated_at"
     t.string   "discription"
     t.integer  "user_id"
+    t.integer  "course_id"
   end
+
+  add_index "presentations", ["course_id"], name: "index_presentations_on_course_id"
 
   create_table "slides", force: true do |t|
     t.integer  "slide_number"
