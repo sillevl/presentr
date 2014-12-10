de5b2f8dbc4f9976a7858dfad2814035a273f5e0
Pieter Desender <Pieter_Desender@hotmail.com>
2014-12-10 11:18:56 +0100

    clean master

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20141203162130) do
 
-    create_table "courses", force: true do |t|
+  create_table "courses", force: true do |t|
     t.string   "name"
     t.string   "description"
     t.datetime "created_at"
@@ -81,17 +81,4 @@ ActiveRecord::Schema.define(version: 20141203162130) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
-  create_table "themes", force: true do |t|
-    t.string   "name"
-    t.text     "code"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "transistions", force: true do |t|
-    t.string   "name"
-    t.text     "code"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 end
