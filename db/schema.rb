7ee5f2bc9faee7d3b8fdebd23515363be3a3ee6d
Stephen D'Hondt <stephendhondt@hotmail.be>
2014-12-10 11:25:38 +0100

    Made a list of courses and indexes.

--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,20 @@ ActiveRecord::Schema.define(version: 20141203151240) do
     t.string   "slide_name"
   end
 
+  create_table "themes", force: true do |t|
+    t.string   "name"
+    t.text     "code"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "transistions", force: true do |t|
+    t.string   "name"
+    t.text     "code"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
@@ -61,20 +75,5 @@ ActiveRecord::Schema.define(version: 20141203151240) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
-
-  create_table "themes", force: true do |t|
-    t.string   "name"
-    t.text     "code"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "transistions", force: true do |t|
-    t.string   "name"
-    t.text     "code"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
 end
