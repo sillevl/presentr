df1f74226db43c2f7b2c76584e8cb35dbee4c94c
Pieter Desender <Pieter_Desender@hotmail.com>
2014-12-16 11:34:42 +0100

    created faundationfrombuilder and changed to default formbuilder

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,11 @@ module ApplicationHelper
     	javascript_tag "Reveal.configure({ slideNumber: true })"
     end
 
+    class FoundationFormBuilder < ActionView::Helpers::FormBuilder 
+    	def submit (value=nil, options={})
+    		options[:class] = "button"
+    		super value, options
+    	end
+    end
+
 end
